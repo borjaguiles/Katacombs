15928cd86805e7fa1437464440843533caf1be92
Borja <bguiles@bavel.es>
2019-12-16 15:24:23 +0100

    test: Tell player theres nothing when looking south

--- Katacombs.Tests/Zones/PlayerShould.cs
+++ Katacombs.Tests/Zones/PlayerShould.cs
@@ -15,5 +15,15 @@ namespace Katacombs.Tests.Zones
             var result = player.Look("N");
             Assert.Equal("I CAN SEE A BRICK BUILDING WITH A SIGN SAYING \"TRUMAN BREWERY\" AND A WOODEN WHITE DOOR", result);
         }
+
+        [Fact]
+        public void TellPlayerTheresNothingWhenLookingSouth()
+        {
+            var zoneConfig = Substitute.For<IZoneConfiguration>();
+            zoneConfig.LookAtDirection(Direction.S).Returns(new[] { "Nothing interesting to look at there!" });
+            var player = new StartingPlayer(zoneConfig);
+            var result = player.Look("S");
+            Assert.Equal("Nothing interesting to look at there!", result);
+        }
     }
 }
